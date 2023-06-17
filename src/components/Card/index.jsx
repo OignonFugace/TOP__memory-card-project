87ea34da8e2c2f1bdcd431a2d3f163561ebe2e32
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-06-17 22:11:38 +0200

    feat(UI): Implement card tilting capability

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -12,24 +12,37 @@ import {
 } from "../../utils/constants";
 import useFitText from "use-fit-text";
 
-function handleClick(card, score, maxScore, dispatch) {
-  if (card.isClicked) {
-    dispatch({ type: SET_STAGE_STATE, payload: { state: STAGE_STATE_LOST } });
-  } else {
-    dispatch({ type: SELECT_CARD, payload: { card: card } });
-    if (score + 1 === maxScore) {
-      dispatch({ type: SET_STAGE_STATE, payload: { state: STAGE_STATE_WON } });
-    } else {
-      dispatch({ type: SET_DISPLAYED_CARDS });
-    }
-  }
-}
-
-function Card({ card }) {
+function Card({ card, isFlipped, setIsFlipped }) {
   const { t, language } = useContext(LanguageContext);
   const { dispatch, score, maxScore } = useContext(GameContext);
   const { fontSize, ref } = useFitText();
   const [renderCount, setRenderCount] = useState(0);
+
+  function handleClick(card) {
+    setIsFlipped(true);
+
+    setTimeout(() => {
+      if (card.isClicked) {
+        dispatch({
+          type: SET_STAGE_STATE,
+          payload: { state: STAGE_STATE_LOST },
+        });
+      } else {
+        dispatch({ type: SELECT_CARD, payload: { card: card } });
+        if (score + 1 === maxScore) {
+          dispatch({
+            type: SET_STAGE_STATE,
+            payload: { state: STAGE_STATE_WON },
+          });
+        } else {
+          dispatch({ type: SET_DISPLAYED_CARDS });
+        }
+      }
+      setTimeout(() => {
+        setIsFlipped(false);
+      }, 500);
+    }, 500);
+  }
 
   useEffect(() => {
     setRenderCount(renderCount + 1);
@@ -37,23 +50,27 @@ function Card({ card }) {
 
   return (
     <div
-      className="card"
-      onClick={() => handleClick(card, score, maxScore, dispatch)}
+      className={`card ${isFlipped ? "is-flipped" : ""}`}
+      onClick={() => handleClick(card)}
       key={renderCount}
     >
-      <div
-        className="card__picture"
-        style={{ backgroundImage: `url(${sampleImg})` }}
-      ></div>
+      <div className="card-face front-face">
+        <div
+          className="card__picture"
+          style={{ backgroundImage: `url(${sampleImg})` }}
+        ></div>
 
-      <div className="card__title">
-        <p
-          ref={ref}
-          style={{ fontSize, height: 34, width: 195, whiteSpace: "nowrap" }}
-        >
-          {card.itemName[t("language")]}
-        </p>
+        <div className="card__title">
+          <p
+            ref={ref}
+            style={{ fontSize, height: 34, width: 195, whiteSpace: "nowrap" }}
+          >
+            {card.itemName[t("language")]}
+          </p>
+        </div>
       </div>
+
+      <div className="card-face back-face">Some Text</div>
     </div>
   );
 }
--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GameContext from "../../context/GameContextProvider";
 import Card from "../Card";
 import "./cardList.css";
@@ -6,12 +6,15 @@ import Tilt from "react-parallax-tilt";
 
 function CardList() {
   const { displayedCards } = useContext(GameContext);
+  const [isDeckFlipped, setIsDeckFlipped] = useState(false);
 
   return (
     <div className="card-list">
       {displayedCards?.map((card) => (
         <Tilt key={card.id} glareEnable={true}>
-          <Card card={card} />
+          <div className="tilt-wrapper">
+            <Card card={card} isFlipped={isDeckFlipped} setIsFlipped={setIsDeckFlipped} />
+          </div>
         </Tilt>
       ))}
     </div>
